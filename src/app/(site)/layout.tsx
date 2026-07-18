14adad92888cda70ea088a6a677201bb6024dda5
mooknayak <diwakar7571@gmail.com>
2026-07-18 10:35:15 +0530

    Update layout.tsx

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import CookieConsent from '@/components/CookieConsent'
+import PushNotificationPrompt from '@/components/PushNotificationPrompt'
 
 export default function SiteLayout({
   children,
@@ -13,6 +14,7 @@ export default function SiteLayout({
       <main className="min-h-screen max-w-5xl mx-auto px-4 py-6">{children}</main>
       <Footer />
       <CookieConsent />
+      <PushNotificationPrompt />
     </>
   )
 }
