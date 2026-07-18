e1924404b2d19a43baf30415e7608d58d5561b83
mooknayak <diwakar7571@gmail.com>
2026-07-18 10:26:57 +0530

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -2,5 +2,6 @@ import { jobPost } from './jobPost'
 import { category } from './category'
 import { organization } from './organization'
 import { subscriber } from './subscriber'
+import { pushSubscriber } from './pushSubscriber'
 
-export const schemaTypes = [jobPost, category, organization, subscriber]
+export const schemaTypes = [jobPost, category, organization, subscriber, pushSubscriber]
